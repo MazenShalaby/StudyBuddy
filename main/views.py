c08c05e2d727f704820c6e56e3f08de6c22b3d03
Mazen Shalaby <waleedmazen85@gmail.com>
2026-04-01 23:00:57 +0200

    (refactor): refactoring room searching in the rooms view logic by using postgres three search classes.

--- main/views.py
+++ main/views.py
@@ -3,87 +3,110 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q, Count
 from django.http import HttpResponse
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Topic, Room, Message
 from .forms import RoomCreationForm, UpdateMessageForm
+
 # Create your views here.
 
 
 def home(request):
     if request.user.is_authenticated:
-        return redirect('main:rooms')
-    return render(request, 'main/home.html', context={})
+        return redirect("main:rooms")
+    return render(request, "main/home.html", context={})
+
 
 ####################################################### [Room] ####################################################
 
-@login_required(login_url='accounts:login')
+
+@login_required(login_url="accounts:login")
 def rooms(request):
-    rooms = Room.objects.select_related('topic')
+    rooms = Room.objects.select_related("topic")
     topics_count = Topic.objects.all().count()
-    topics = Topic.objects.prefetch_related('topic_rooms')[:3] # limiting the topics to only 3 to be shown.
-    recent_activity_messages = Message.objects.select_related('room')
-    q = request.GET.get('q', '')
-    
-    if request.method == 'GET':
-        rooms = rooms.filter(
-            Q(name__icontains=q)|
-            Q(host__username__icontains=q)|
-            Q(topic__name__icontains=q)
+    topics = Topic.objects.prefetch_related("topic_rooms")[:3]  # limiting the topics to only 3 to be shown.
+    recent_activity_messages = Message.objects.select_related("room")
+    # q = request.GET.get('q', '')
+    # if request.method == 'GET':
+    #     rooms = rooms.filter(
+    #         Q(name__icontains=q)|
+    #         Q(host__username__icontains=q)|
+    #         Q(topic__name__icontains=q)
+    #         )
+
+    query = request.GET.get("q")
+    if request.method == "GET" and query:
+        search_vector = SearchVector("name", "host__username", "topic__name")
+        search_query = SearchQuery(query)
+        search_rank = SearchRank(search_vector, search_query)
+        rooms = (
+            Room.objects.annotate(
+                search=search_vector,
+                rank=search_rank,
             )
-    
-    context = {'rooms': rooms, 'topics_count':topics_count, 'topics': topics, 'recent_activity_messages': recent_activity_messages}
-    return render(request, 'main/rooms.html', context)
+            .filter(search=search_query)
+            .order_by("-rank")
+        )
+
+    context = {
+        "rooms": rooms,
+        "topics_count": topics_count,
+        "topics": topics,
+        "recent_activity_messages": recent_activity_messages,
+    }
+    return render(request, "main/rooms.html", context)
 
 
-@login_required(login_url='accounts:login')
+@login_required(login_url="accounts:login")
 def room(request, room_id):
     room = get_object_or_404(Room, id=room_id)
     room_messages = room.room_messages.all()
 
-    
-    if request.method == 'POST':
-        message_body = request.POST.get('msg', '')
+    if request.method == "POST":
+        message_body = request.POST.get("msg", "")
         if message_body:
             new_message = Message.objects.create(
                 user=request.user,
-                room = room,
-                body = message_body,
+                room=room,
+                body=message_body,
             )
             room.participants.add(new_message.user)
         else:
-            messages.error(request, 'Message body must have a content and can\'t be left blank!') # show a msg to the user !
-    context = {'room': room, 'room_messages': room_messages}
-    return render(request, 'main/room.html', context)
+            messages.error(
+                request, "Message body must have a content and can't be left blank!"
+            )  # show a msg to the user !
+    context = {"room": room, "room_messages": room_messages}
+    return render(request, "main/room.html", context)
 
 
-@login_required(login_url='accounts:login')
+@login_required(login_url="accounts:login")
 def create_room(request):
-    
-    page = 'create'
+
+    page = "create"
     topics = Topic.objects.all()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RoomCreationForm(data=request.POST)
         if form.is_valid():
             new_form = form.save(commit=False)
             new_form.host = request.user
             new_form.save()
-            return redirect('main:rooms')
+            return redirect("main:rooms")
         else:
             messages.error(request, "An error has been occured during room creation!")
     else:
         form = RoomCreationForm()
-        
-    context = {'page': page, 'topics': topics, 'form': form}
-    return render(request, 'main/create_update_room.html', context)
+
+    context = {"page": page, "topics": topics, "form": form}
+    return render(request, "main/create_update_room.html", context)
 
 
-@login_required(login_url='accounts:login')
+@login_required(login_url="accounts:login")
 def update_room(request, room_id):
-    
+
     room = get_object_or_404(Room, id=room_id)
     form = RoomCreationForm(instance=room)
     if request.user == room.host:
-        if request.method == 'POST':
+        if request.method == "POST":
             form = RoomCreationForm(data=request.POST, instance=room)
             if form.is_valid():
                 form.save()
@@ -92,78 +115,82 @@ def update_room(request, room_id):
                 return HttpResponse("An error has been occured during room update!")
     else:
         return HttpResponse("You are not allowed to perform this action!")
-        
-    context = {'form': form}
-    return render(request, 'main/create_update_room.html', context)
+
+    context = {"form": form}
+    return render(request, "main/create_update_room.html", context)
 
 
-@login_required(login_url='accounts:login')
+@login_required(login_url="accounts:login")
 def delete_room(request, room_id):
-    
+
     room = get_object_or_404(Room, id=room_id)
     if request.user == room.host:
-        if request.method == 'POST':
+        if request.method == "POST":
             room.delete()
-            return redirect('main:rooms')
+            return redirect("main:rooms")
     else:
         return HttpResponse("You are not allowed to perform this action!")
-        
-    context = {'room': room}
-    return render(request, 'main/delete_room.html', context)
+
+    context = {"room": room}
+    return render(request, "main/delete_room.html", context)
+
 
 ####################################################### [Browse Topics] ####################################################
 
-@login_required(login_url='accounts:login')
+
+@login_required(login_url="accounts:login")
 def browse_topics(request):
 
     topics = Topic.objects.all()
-    q = request.GET.get('q', '')
-    if request.method == 'GET':
-        topics = (
-            Topic.objects
-            .filter(name__icontains=q)
-            .annotate(rooms_count=Count('topic_rooms')) # For preventing N+1 queries while counting each topic's rooms, In the template we use that annotate attr {{topic.rooms_count}} if our temp varibale in the loop is topic. 
-        )
-    
-    context = {'topics': topics}
-    return render(request, 'main/browse_topics.html', context)
+    q = request.GET.get("q", "")
+    if request.method == "GET":
+        topics = Topic.objects.filter(name__icontains=q).annotate(
+            rooms_count=Count("topic_rooms")
+        )  # For preventing N+1 queries while counting each topic's rooms, In the template we use that annotate attr {{topic.rooms_count}} if our temp varibale in the loop is topic.
+
+    context = {"topics": topics}
+    return render(request, "main/browse_topics.html", context)
+
 
 ####################################################### [Message] ####################################################
 
-@login_required(login_url='main:login')
+
+@login_required(login_url="main:login")
 def delete_message(request, msg_id):
-    
+
     message = get_object_or_404(Message, id=msg_id)
     room = message.room
     if request.user == message.user:
-        if request.method == 'POST':
+        if request.method == "POST":
             message.delete()
-            return redirect('main:rooms')
+            return redirect("main:rooms")
     else:
         return HttpResponse("Your are not allowed to perform his action!")
-    
-    context = {'message': message, 'room': room}
-    return render(request, 'main/delete_message.html', context)
+
+    context = {"message": message, "room": room}
+    return render(request, "main/delete_message.html", context)
 
 
-@login_required(login_url='main:login')
+@login_required(login_url="main:login")
 def update_message(request, msg_id):
-    
+
     message = get_object_or_404(Message, id=msg_id)
     room = message.room
     form = UpdateMessageForm(instance=message)
     if request.user == message.user:
-        if request.method == 'POST':
+        if request.method == "POST":
             form = UpdateMessageForm(data=request.POST, instance=message)
             if form.is_valid():
                 user = form.save(commit=False)
                 user.user = request.user
                 user.save()
-                return redirect('main:room', room_id=room.id)
+                return redirect("main:room", room_id=room.id)
             else:
-                messages.error(request, "An error has been occured during updating the message!")
+                messages.error(
+                    request, "An error has been occured during updating the message!"
+                )
     else:
         return HttpResponse("Your are not allowed to perform his action!")
-    
-    context = {'message': message, 'room': room, 'form': form}
-    return render(request, 'main/update_message.html', context)
+
+    context = {"message": message, "room": room, "form": form}
+    return render(request, "main/update_message.html", context)
